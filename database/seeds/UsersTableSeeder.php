cc7bbb0426d124ff3bf2f43eed99133456b97ec4
Angel <pixan-angel@pixan.io>
2020-04-04 16:39:02 -0600

    mail config fixed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,35 +12,36 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        User::truncate();
         User::create([
             'name' => 'su',
             'email' => 'su@test.com',
-            'password' => bcrypt('123'),
+            'password' => bcrypt('123123'),
         ]);
         User::create([
             'name' => 'Administrador 1',
             'email' => 'admin1@pldhamexico.org',
-            'password' => bcrypt('Admin2019abc!'),
+            'password' => bcrypt('detp!6C84gIC'),
         ]);
         User::create([
             'name' => 'Administrador 2',
             'email' => 'admin2@pldhamexico.org',
-            'password' => bcrypt('Admin2027abc!zn'),
+            'password' => bcrypt('vKDa0CA#@qOp'),
         ]);
         User::create([
             'name' => 'Administrador 3',
             'email' => 'admin3@pldhamexico.org',
-            'password' => bcrypt('Admin2020!37b'),
+            'password' => bcrypt('e3CRRx1iD!CL'),
         ]);
         User::create([
             'name' => 'Administrador 4',
             'email' => 'admin4@pldhamexico.org',
-            'password' => bcrypt('Admin2018dbca'),
+            'password' => bcrypt('lHAPh&QFA#!^'),
         ]);
         User::create([
             'name' => 'Administrador 5',
             'email' => 'admin5@pldhamexico.org',
-            'password' => bcrypt('Admin3892019'),
+            'password' => bcrypt('i7n^!6Z!nty#'),
         ]);
     }
 }
